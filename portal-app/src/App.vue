b1d2e08920ccb04dc50d8c390032814e8892ef9e
Nix <xinxintao@foxmail.com>
2020-02-27 14:18:43 +0800

    use element-ui

--- portal-app/src/App.vue
+++ portal-app/src/App.vue
@@ -1,9 +1,26 @@
 <template>
   <div id="main-root">
-    <!-- loading -->
-    <div v-if="loading">loading</div>
-    <!-- 子应用盒子 -->
-    <div id="root-view" class="app-view-box" v-html="content"></div>
+    <el-container>
+      <el-aside width="200px">
+        <el-menu default-active="app1" class="el-menu-vertical-demo" router>
+          <el-menu-item index="app1">
+            <i class="el-icon-menu"></i>
+            <span slot="title">子应用1</span>
+          </el-menu-item>
+          <el-menu-item index="app2">
+            <i class="el-icon-document"></i>
+            <span slot="title">子应用2</span>
+          </el-menu-item>
+        </el-menu>
+      </el-aside>
+      <el-container>
+        <el-header>Header</el-header>
+        <el-main v-loading="loading">
+          <!-- 子应用盒子 -->
+          <div id="root-view" class="app-view-box" v-html="content"></div>
+        </el-main>
+      </el-container>
+    </el-container>
   </div>
 </template>
 
@@ -16,3 +33,28 @@ export default {
   }
 };
 </script>
+<style>
+#main-root {
+  height: 100%;
+}
+.el-header {
+  background-color: #b3c0d1;
+  color: #333;
+  text-align: center;
+}
+
+.el-aside {
+  color: #333;
+  text-align: center;
+}
+
+.el-main {
+  background-color: #e9eef3;
+  color: #333;
+  text-align: center;
+}
+
+.el-container {
+  height: 100%;
+}
+</style>
--- portal-app/src/main.js
+++ portal-app/src/main.js
@@ -1,9 +1,12 @@
 import Vue from "vue"
 import App from "./App.vue"
 import router from "./router"
-
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 import { registerMicroApps, setDefaultMountApp, start } from "qiankun"
 Vue.config.productionTip = false
+Vue.use(ElementUI);
+
 let app = null;
 /**
  * 渲染函数
