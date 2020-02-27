3085375fa7abd32aa1a1a1f2689f21599c3aed53
Nix <xinxintao@foxmail.com>
2020-02-27 13:51:36 +0800

    update sub-app-1

--- sub-app-1/src/main.js
+++ sub-app-1/src/main.js
@@ -1,8 +1,42 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import App from './App.vue';
+import routes from './router';
+import './public-path';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+let router = null;
+let instance = null;
+
+function render() {
+	router = new VueRouter({
+		base: window.__POWERED_BY_QIANKUN__ ? '/app1' : '/',
+		mode: 'history',
+		routes,
+	});
+
+	instance = new Vue({
+		router,
+		render: h => h(App),
+	}).$mount('#app');
+}
+
+if (!window.__POWERED_BY_QIANKUN__) {
+	render();
+}
+
+export async function bootstrap() {
+	console.log('vue app bootstraped');
+}
+
+export async function mount(props) {
+	console.log('props from main app', props);
+	render();
+}
+
+export async function unmount() {
+	instance.$destroy();
+	instance = null;
+	router = null;
+}
---
+++ sub-app-1/src/router.js
@@ -0,0 +1,22 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+
+Vue.use(VueRouter);
+
+const routes = [
+	{
+		path: '/',
+		name: 'home',
+		redirect: 'hello'
+	},
+	{
+		path: '/hello',
+		name: 'hello',
+		// route level code-splitting
+		// this generates a separate chunk (about.[hash].js) for this route
+		// which is lazy-loaded when the route is visited.
+		component: () => import(/* webpackChunkName: "about" */ '@/views/HelloWorld.vue'),
+	},
+];
+
+export default routes;
